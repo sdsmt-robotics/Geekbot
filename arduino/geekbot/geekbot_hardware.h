5d96132f4a85af5006cf8af44470c0f7ff159000
Remington Bullis <remington.bullis@mines.sdsmt.edu>
2016-09-24 13:07:43 -0600

    Changed the IR_SERVO to pin 3

--- arduino/geekbot/geekbot_hardware.h
+++ arduino/geekbot/geekbot_hardware.h
@@ -6,12 +6,12 @@
 #define BUZZER_PIN      2 // Which pin goes where!
 #define LED_LEFT_PIN    7
 #define LED_RIGHT_PIN   4
-#define SERVO_IR_PIN    12
+#define SERVO_IR_PIN    3
 #define SERVO_LEFT_PIN  11
 #define SERVO_RIGHT_PIN 9
 #define SERVO_CAM_HORIZONTAL_PIN  6
 #define SERVO_CAM_VERTICAL_PIN    5
-#define SERVO_CAM_ROLL_PIN        3
+#define SERVO_CAM_ROLL_PIN        10
 
 #define BUTTON_PIN      8
 #define IR_PIN          A0
